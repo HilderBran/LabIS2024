0647a5f44928fc1477a575a1e85fdec5a18312f6
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-09-21 10:32:10 -0600

    Arquitectura del Proyecto e instalaciones necesarias

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor iniciando en el puerto', app.get('port'));
+});
