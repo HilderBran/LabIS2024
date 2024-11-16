79d65f96946e8907e35e0ea28c7c7b313934106e
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-11-16 10:01:01 -0600

    Matenimiento de estudiantes

--- src/repositories/EstudianteRepository.js
+++ src/repositories/EstudianteRepository.js
@@ -8,6 +8,38 @@ module.exports = {
         } catch (error) {
             console.error('Error un problema al consultar la lista de los estudiantes', error);
             return error;
-        } 
+        }
+    },
+
+    // Eliminar un estudiante
+    eliminarEstudiante: async (idestudiante) => {
+        try {
+            const result = await pool.query('DELETE FROM estudiantes WHERE idestudiante = ?', [idestudiante]);
+            return result.affectedRows > 0;
+        } catch (error) {
+            console.error('Erro al eliminar el registro', error);
+        }
+    },
+
+    // Insertar un estudiante
+    insertarEstudiante: async (estudiante) => {
+        try {
+            const result = await pool.query('INSERT INTO estudiantes SET ?', [estudiante]);
+            return result.affectedRows > 0;
+        } catch (error) {
+            console.error('Error al insertar el registro', error);
+        }
+    },
+
+    // Actualizar un estudiante
+    actualizarEstudiante: async (estudiante, idestudiante) => {
+        try {
+            const result = await pool.query('UPDATE estudiantes SET ? WHERE idestudiante = ?', [estudiante, idestudiante]);
+            return result.affectedRows > 0;
+        } catch (error) {
+            console.error('Error al actualizar el registro', error);
+        }
     }
-};
+}
+
+
